64a8402fa5c3080e586114f192d1084ec121a304
thseo <thseo@spectra.co.kr>
2022-05-09 14:54:28 +0900

    first commit - jpa, oauth2-client, security, validation, web, websocket, lombok, mysql

---
+++ src/main/java/com/indien/indien_backend/IndienBackendApplication.java
@@ -0,0 +1,15 @@
+package com.indien.indien_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndienBackendApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(IndienBackendApplication.class, args);
+    }
+
+}
