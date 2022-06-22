d1c1bf8eee56e7dc53e2750ea5e1654ed2bee6d7
smallgood <smallgood@rssolutions.kr>
2022-06-22 13:34:30 +0900

    기본 세팅

---
+++ src/main/java/com/indien/indien_backend/api/MainController.java
@@ -0,0 +1,25 @@
+package com.indien.indien_backend.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping(value="/")
+	public Map<String, String> home() {
+		
+		// 로그인 여부 확인해서 유저 정보 가져오기
+		System.out.println("진입");
+		// 영화 목록 가져오기
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("res", "홈화면진입");
+		
+		return  map;
+	}
+	
+	
+}
